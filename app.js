5d0b9c01e09297fa47cc5e04fafc827d1f4920d3
efrozkhan <38079731+efrozkhan@users.noreply.github.com>
2026-05-07 17:01:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Node.js app running from GitHub Actions");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("GitHub Actions CI Working");
+});
+
+console.log("Express app loaded successfully");
